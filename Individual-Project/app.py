4b9f072ca2bfe9d0ebbef07db326e963ad0c3282
maayansa24-meet <maayansapp@gmail.com>
2023-07-27 00:02:11 +0300

    not finished

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -10,6 +10,7 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 config = {
   "apiKey": "AIzaSyAzZoEy0H-H6plWLDHc6swXB91IOu36Yoc",
   "authDomain": "cs-prog.firebaseapp.com",
+  "databaseURL": "https://cs-prog-default-rtdb.europe-west1.firebasedatabase.app",
   "projectId": "cs-prog",
   "storageBucket": "cs-prog.appspot.com",
   "messagingSenderId": "1020287998690",
@@ -23,30 +24,71 @@ auth = firebase.auth()
 db = firebase.database()
 
 
-
-@app.route('/signup', methods=['GET', 'POST'])
+# Route for the signup page
+@app.route('/', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        # Get the input values from the form
         try:
+            userName = request.form['userName']
+            email = request.form['email']
+            password = request.form['password']
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            full_name = request.form['full_name']
-            username = request.form['username']
-            bio = request.form['bio'][:280]  # Truncate to a maximum of 280 characters
-
-            # Create a dictionary with user information
-            user = {
-                'full_name': full_name,
-                'username': username,
-                'bio': bio
-            }
-
-            # Add the user to the database using their uid (retrieve it through login_session)
-            uid = login_session['user']['localId']  # Replace with the actual way of retrieving the uid
+            uid = login_session['user']['localId']
+            user = {"email": email, "password":password,"name":userName}
             db.child('Users').child(uid).set(user)
-            return redirect(url_for('/'))
+            return render_template('sign_in.html')  
         except:
             error = "Authentication failed"
+            print(error)
+    return render_template('sign_up.html')  
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+   if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('home'))
+       except:
+           error = "Authentication failed"
+           print(error)
+   return render_template("sign_in.html")
+@app.route('/home')
+def home():
+    uName = db.child('Users').child('user').child('name').get().val()
+    return render_template('home.html', uName = uName)
+@app.route('/products')
+def products():
+    return render_template('products.html')
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+
+@app.route('/add_to_cart', methods=['GET', 'POST'])
+def add_cart():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        UID = login_session['user']['localId']
+        to_add = {
+            'name': name,
+            'price': price
+        }
+        db.child('Cart').push(to_add)
+        return redirect('cart') 
+    return render_template('cart.html')
+
+# Route for displaying all tweets
+@app.route('/all_tweets')
+def all_tweets():
+    # Get all the tweets from the database
+    tweets = db.child('Tweets').get()
+
+    return render_template('tweets.html', tweets = tweets)
+
+
 
 
 
